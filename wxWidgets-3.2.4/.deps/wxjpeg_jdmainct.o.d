a7ed2e9884484d1ccaca40662a999b8c313d6884
mranv <iamanubhavgain@gmail.com>
2024-02-20 03:42:49 +0530

    Apple Silicon

---
+++ wxWidgets-3.2.4/.deps/wxjpeg_jdmainct.o.d
@@ -0,0 +1,5 @@
+wxjpeg_jdmainct.o: src/jpeg/jdmainct.c src/jpeg/jinclude.h \
+  src/jpeg/jconfig.h \
+  /Users/mranv/Downloads/wxWidgets-3.2.4/lib/wx/include/osx_cocoa-unicode-static-3.2/wx/setup.h \
+  src/jpeg/jpeglib.h src/jpeg/jmorecfg.h src/jpeg/jpegint.h \
+  src/jpeg/jerror.h
---
+++ wxWidgets-3.2.4/.deps/wxjpeg_jerror.o.d
@@ -0,0 +1,4 @@
+wxjpeg_jerror.o: src/jpeg/jerror.c src/jpeg/jinclude.h src/jpeg/jconfig.h \
+  /Users/mranv/Downloads/wxWidgets-3.2.4/lib/wx/include/osx_cocoa-unicode-static-3.2/wx/setup.h \
+  src/jpeg/jpeglib.h src/jpeg/jmorecfg.h src/jpeg/jversion.h \
+  src/jpeg/jerror.h
